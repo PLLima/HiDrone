86e87190295ad9bc3efcdc58a87722fe61e99947
PLLima <74931964+PLLima@users.noreply.github.com>
2025-05-07 11:49:35 -0300

    chore: add api function to get user from database

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::get(uri: '/users', action: function(): AnonymousResourceCollection {
+    return UserResource::collection(resource: User::all());
 });
