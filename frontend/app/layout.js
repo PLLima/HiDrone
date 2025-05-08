a53fbffbc39c9b9551a81731359a896d91de7bb2
jGp34 <150202089+jGp34@users.noreply.github.com>
2025-05-07 21:39:08 -0300

    fix: change lang to english

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
