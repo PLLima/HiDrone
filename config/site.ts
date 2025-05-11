fbbb9c7b9c791fd773ad37bc2153a6bb81cb2114
JoaoVitorBaggio <72949356+JoaoVitorBaggio@users.noreply.github.com>
2025-05-11 15:29:41 -0300

    Feat: Clean homepage

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  description: "Homepage currently missing 😅",
   navItems: [
     {
       label: "Hire",
