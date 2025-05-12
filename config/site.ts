78ff2dd7b1843e70fd6c7bef51e82f17692bb45c
JoaoVitorBaggio <72949356+JoaoVitorBaggio@users.noreply.github.com>
2025-05-12 16:47:47 -0300

    feat: change the tittle of the web tab to "HiDrone"

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
+  name: "HiDrone",
   description: "Homepage currently missing 😅",
   navItems: [
     {
