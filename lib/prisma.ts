6f421f139893bddbcd4d58b81bd8f4581589ce5f
PLLima <74931964+PLLima@users.noreply.github.com>
2025-05-13 19:18:00 -0300

    fix: try to make database updates work on deployed version

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-let prisma: PrismaClient;
+let prisma: any;
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient().$extends(withAccelerate());
 } else {
   if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient();
+    (global as any).prisma = new PrismaClient().$extends(withAccelerate());
   }
   prisma = (global as any).prisma;
 }
