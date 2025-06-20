4eed78541c8c251ad5658f6aedd1d708753ca5e6
JoaoVitorBaggio <72949356+JoaoVitorBaggio@users.noreply.github.com>
2025-06-20 14:22:50 -0300

    feat: change drone tab in hire page to singular

--- app/hire/page.tsx
+++ app/hire/page.tsx
@@ -8,7 +8,7 @@ export default function SupplyPage() {
   return (
     <section className="flex flex-col items-center justify-center gap-0 py-2 px-6 w-full max-w-screen-2xl mx-auto">
       <Tabs aria-label="Options">
-        <Tab key="drones" title="Drones">
+        <Tab key="drone" title="Drone">
           <SearchDronesPage />
         </Tab>
         <Tab key="route" title="Route">
